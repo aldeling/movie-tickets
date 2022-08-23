f5cdb496bcc37d7f4eb4cd4f6601e4563750cfb8
Athea DeLing <almdeling@gmail.com>
2022-08-23 14:40:05 -0700

    add html with movies, times, and age input, final price of ticket determined by age
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>

--- README.md
+++ README.md
@@ -2,5 +2,5 @@ Describe: Ticket()
 
 Test: It should return the movie, time, age, and price
 Code:
-let testTicket = new Ticket("Princess Bride", "12:00pm", "20", "$12.00");
+let testTicket = new Tickets;
 Expected Output: Ticket{movieName: "Princess Bride", movieTime: "12:00pm", movieAge: "20", moviePrice: "$12.00"}
--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,29 @@ function Ticket(movieName, movieTime, movieAge, moviePrice) {
   this.movieAge = movieAge;
   this.moviePrice = moviePrice;
 }
+
+// UI logic
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  let moviePrice = new function(e) {
+    let a = document.getElementById("new-movie-age").value
+    let b = "$5.00"
+    let c = "$10.00"
+    let d = "$7.00"
+
+    if (a <= 5 && a >=1) {
+      document.querySelector(".movie-price").innerText = b
+    }else if (a > 5 && a < 55) {
+      document.querySelector(".movie-price").innerText = c
+    }else if (a >= 55) {
+      document.querySelector(".movie-price").innerText = d
+    }else {
+      document.querySelector(".movie-price").innerText = "Error please input a reasonable age!"
+    }
+  };
+}
+
+window.addEventListener("load", function (){
+  document.querySelector("form#new-ticket").addEventListener("submit", handleFormSubmission);
+});
